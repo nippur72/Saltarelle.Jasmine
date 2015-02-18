3043fb48aecd03dd76a2642a6f4bdd6bcff93af7
Brodie Gilmour <brodie.gilmour@webapplicationsuk.com>
2015-02-18 17:00:58 +0000

    Added support for async tests in Jasmine 2.1.

--- Jasmine/Jasmine.cs
+++ Jasmine/Jasmine.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Serialization;
+using System.Threading.Tasks;
 
 [assembly: PreserveMemberCase]
 
@@ -12,106 +13,165 @@ using System.Serialization;
 
 namespace Jasmine
 {
-   [Imported]
-   public class Matcher
-   {
-      public bool toBe(object expected) { return false; }
-      public bool toEqual(object expected) { return false; }
-      public bool toMatch(string expected) { return false; }
-      public bool toBeDefined() { return false; }
-      public bool toBeUndefined() { return false; }
-      public bool toBeNull() { return false; }
-      public bool toBeNaN() { return false; }
-      public bool toBeTruthy() { return false; }
-      public bool toBeFalsy() { return false; }
-      public bool toHaveBeenCalled() { return false; }      
-      [ExpandParams] public bool toHaveBeenCalledWith(params object[] par) { return false; }      
-      public bool toContain(object expected) { return false; }
-      public bool toBeLessThan(object expected) { return false; }
-      public bool toBeGreaterThan(object expected) { return false; }
-      public bool toBeCloseTo(object expected, int precision) { return false; }
-      public bool toThrow() { return false; }
-      public bool toThrow(object expected) { return false; }
+    [Imported]
+    public class Matcher
+    {
+        public bool toBe(object expected) { return false; }
+        public bool toEqual(object expected) { return false; }
+        public bool toMatch(string expected) { return false; }
+        public bool toBeDefined() { return false; }
+        public bool toBeUndefined() { return false; }
+        public bool toBeNull() { return false; }
+        public bool toBeNaN() { return false; }
+        public bool toBeTruthy() { return false; }
+        public bool toBeFalsy() { return false; }
+        public bool toHaveBeenCalled() { return false; }
+        [ExpandParams]
+        public bool toHaveBeenCalledWith(params object[] par) { return false; }
+        public bool toContain(object expected) { return false; }
+        public bool toBeLessThan(object expected) { return false; }
+        public bool toBeGreaterThan(object expected) { return false; }
+        public bool toBeCloseTo(object expected, int precision) { return false; }
+        public bool toThrow() { return false; }
+        public bool toThrow(object expected) { return false; }
 
-      [IntrinsicProperty]
-      public Matcher not { get { return null;} }
+        [IntrinsicProperty]
+        public Matcher not { get { return null; } }
 
-      // any matcher
+        // any matcher
 
-      // typeof matcher?
-   }
+        // typeof matcher?
+    }
 
-   [Imported]
-   public class SpyCall
-   {
-      public object[] args;
-   }
-   
-   [Imported]
-   public class Spy
-   {
-      public string identity;
-      public SpyCall mostRecentCall;
-      public SpyCall[] calls; 
-      public Spy andCallThrough() { return null; }     
-      public Spy andReturn(object o) { return null; }     
-      public Spy andCallFake<T>(Func<T> function) { return null; }     
-      [InlineCode("{this}({*args})")]
-      public void Call(params object[] args) {}
-   }
+    [Imported]
+    public class SpyCall
+    {
+        public object[] args;
+    }
 
-   [IgnoreNamespace]
-   [Imported]
-   [ScriptName("Object")]     
-   public class JasmineSuite
-   {
-      [InlineCode("{}")] public JasmineSuite() { }
+    [Imported]
+    public class Spy
+    {
+        public string identity;
+        public SpyCall mostRecentCall;
+        public SpyCall[] calls;
+        public Spy andCallThrough() { return null; }
+        public Spy andReturn(object o) { return null; }
+        public Spy andCallFake<T>(Func<T> function) { return null; }
+        [InlineCode("{this}({*args})")]
+        public void Call(params object[] args) { }
+    }
 
-      [InlineCode("describe({description},{specDefinitions})")]
-      public static void describe(string description, Action specDefinitions) {}
+    [IgnoreNamespace]
+    [Imported]
+    [ScriptName("Object")]
+    public class JasmineSuite
+    {
+        [InlineCode("{}")]
+        public JasmineSuite() { }
 
-      [InlineCode("xdescribe({description},{specDefinitions})")]
-      public static void xdescribe(string description, Action specDefinitions) {}
+        [InlineCode("describe({description},{specDefinitions})")]
+        public static void describe(string description, Action specDefinitions) { }
 
-      [InlineCode("it({desc},{func})")]
-      public static void it(string desc, Action func) {}
+        [InlineCode("fdescribe({description},{specDefinitions})")]
+        public static void fdescribe(string description, Action specDefinitions) { }
 
-      [InlineCode("xit({desc},{func})")]
-      public static void xit(string desc, Action func) {}   
+        [InlineCode("xdescribe({description},{specDefinitions})")]
+        public static void xdescribe(string description, Action specDefinitions) { }
 
-      [InlineCode("expect({o})")]
-      public Matcher expect(object o) { return null; }
+        [InlineCode("it({desc},{func})")]
+        public static void it(string desc, Action func) { }
 
-      [InlineCode("beforeEach({func})")]
-      public static void beforeEach(Action func) { }
+        [InlineCode("it({desc},{func})")]
+        public static void it(string desc, Action<Action> func) { }
 
-      [InlineCode("afterEach({func})")]
-      public static void afterEach(Action func) { }
+        [InlineCode("it({desc}, ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void it(string desc, Func<Task> func) { }
 
-      [InlineCode("spyOn({o},{methodname})")]
-      public static Spy spyOn(object o, string methodname) { return null; }
+        [InlineCode("fit({desc},{func})")]
+        public static void fit(string desc, Action func) { }
 
-      [InlineCode("jasmine.createSpy({name})")]
-      public Spy createSpy(string name) { return null; }
-      
-      [InlineCode("jasmine.createSpyObj({name},{args})")]
-      public Spy createSpyObj(string name, string[] args) { return null; }
+        [InlineCode("fit({desc},{func})")]
+        public static void fit(string desc, Action<Action> func) { }
 
-      // clock mock
+        [InlineCode("fit({desc}, ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void fit(string desc, Func<Task> func) { }
 
-      // async support
-      [InlineCode("runs({func})")]
-      public static void runs(Action func) {}
+        [InlineCode("xit({desc},{func})")]
+        public static void xit(string desc, Action func) { }
 
-      [InlineCode("waitsFor({func},{failure_message},{timeout})")]
-      public static void waitsFor(Func<bool> func, string failure_message, int timeout) { }
+        [InlineCode("xit({desc},{func})")]
+        public static void xit(string desc, Action<Action> func) { }
 
-      [InlineCode("waitsFor({func},'',{timeout})")]
-      public static void waitsFor(Func<bool> func, int timeout) { }
+        [InlineCode("xit({desc}, ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void xit(string desc, Func<Task> func) { }
 
-      [InlineCode("waitsFor({func})")]
-      public static void waitsFor(Func<bool> func) { }
-   }
+        [InlineCode("pending()")]
+        public void pending() { }
+        
+        [InlineCode("expect({o})")]
+        public Matcher expect(object o) { return null; }
+
+        [InlineCode("beforeEach({func})")]
+        public static void beforeEach(Action func) { }
+
+        [InlineCode("beforeEach({func})")]
+        public static void beforeEach(Action<Action> func) { }
+
+        [InlineCode("beforeEach(ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void beforeEach(Func<Task> func) { }
+
+        [InlineCode("afterEach({func})")]
+        public static void afterEach(Action func) { }
+
+        [InlineCode("afterEach({func})")]
+        public static void afterEach(Action<Action> func) { }
+
+        [InlineCode("afterEach(ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void afterEach(Func<Task> func) { }
+
+        [InlineCode("beforeAll({func})")]
+        public static void beforeAll(Action func) { }
+
+        [InlineCode("beforeAll({func})")]
+        public static void beforeAll(Action<Action> func) { }
+
+        [InlineCode("beforeAll(ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void beforeAll(Func<Task> func) { }
+
+        [InlineCode("afterAll({func})")]
+        public static void afterAll(Action func) { }
+
+        [InlineCode("afterAll({func})")]
+        public static void afterAll(Action<Action> func) { }
+
+        [InlineCode("afterAll(ss.mkdel(this, function(done) {{ {func}().continueWith(done); }}))")]
+        public static void afterAll(Func<Task> func) { }
+
+        [InlineCode("spyOn({o},{methodname})")]
+        public static Spy spyOn(object o, string methodname) { return null; }
+
+        [InlineCode("jasmine.createSpy({name})")]
+        public Spy createSpy(string name) { return null; }
+
+        [InlineCode("jasmine.createSpyObj({name},{args})")]
+        public Spy createSpyObj(string name, string[] args) { return null; }
+
+        // clock mock
+
+        // async support
+        [InlineCode("runs({func})")]
+        public static void runs(Action func) { }
+
+        [InlineCode("waitsFor({func},{failure_message},{timeout})")]
+        public static void waitsFor(Func<bool> func, string failure_message, int timeout) { }
+
+        [InlineCode("waitsFor({func},'',{timeout})")]
+        public static void waitsFor(Func<bool> func, int timeout) { }
+
+        [InlineCode("waitsFor({func})")]
+        public static void waitsFor(Func<bool> func) { }
+    }
 }
 
 
